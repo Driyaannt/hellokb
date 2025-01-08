4da0104e7ee099a4984da9ae596aa3731266f649
Driyaannt <49848545+Driyaannt@users.noreply.github.com>
2025-01-08 21:26:32 +0700

    change browser name

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Kbkuu</title>
+    <title>Hello Kb</title>
 
     <!-- Favicon -->
 {{-- <link rel="shortcut icon" type="image/png" href="{{ asset('dist/images/logos/favicon.ico') }}" />   --}}
